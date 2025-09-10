4b40670ea861104ba49e0737d545be697afdb9fa
jonathanhansson <jonathanhanssondg@gmail.com>
2025-09-10 20:59:16 +0200

    api, constants and data_processing, not finished yet

---
+++ code_alongs/08_fastapi_crud/data_processing.py
@@ -0,0 +1,40 @@
+from constants import DATA_PATH
+import json
+from pprint import pprint
+from pydantic import BaseModel, Field
+
+def read_json(filename: str):
+    with open(DATA_PATH / filename, "r") as file:
+        data = json.read(file)
+    return data
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    year: int = Field(gt = 1500, lt = 2026)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 11,
+                "title": "Learn with AIgineer",
+                "author": "Kokchun Giang",
+                "year": 2025
+            }
+        } 
+    }
+
+class Library(BaseModel):
+    name: str
+    books: list[Book]
+
+
+def library_data(filename):
+    # deserializes library json data into a library model
+    json_data = read_json(filename)
+    return Library.model_validate(json_data)
+
+if __name__ == "__main__":
+    data = library_data("../../data/library.json")
+    print(data)
