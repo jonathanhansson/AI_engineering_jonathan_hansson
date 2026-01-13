6e4376dd12f63cc28e66b4be7a536b4a51cc2ccd
jonathanhansson <jonathanhanssondg@gmail.com>
2026-01-13 13:27:25 +0100

    Small change

---
+++ code_alongs/11b_pydanticai_fastapi/api.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from utils import query_duckdb
+from agents import movie_agent
+from data_models import Prompt
+
+app = FastAPI()
+
+@app.get("/movies")
+async def read_movies():
+    movies = query_duckdb("FROM movies;")
+
+    return movies.to_dict(orient="records")
+
+@app.post("/create_movie")
+async def create_movie(query: Prompt):
+    result = await movie_agent.run(query.prompt)
+
+    movie = result.output
+
+    # db logic to save movie
+    query_duckdb(
+        "INSERT INTO movies VALUES (movie.title, movie.year, movie.genre, movie.rating)",
+        parameters=[]
+    )
+
+    return movie.model_dump()
---
+++ code_alongs/11b_pydanticai_fastapi/setup_db.py
@@ -0,0 +1,15 @@
+from utils import query_duckdb
+
+if __name__ == "__main__":
+    query_duckdb("""
+        CREATE TABLE IF NOT EXISTS movies (
+                title TEXT,
+                year INTEGER,
+                genre TEXT,
+                rating TINYINT
+        );
+    """)
+
+    # query_duckdb("INSERT INTO movies VALUES ('Titanic', 1997, 'romance', 5)")
+
+    print(query_duckdb("desc table movies;"))
